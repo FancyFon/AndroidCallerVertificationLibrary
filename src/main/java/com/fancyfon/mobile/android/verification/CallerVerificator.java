1bba376eb863f9dc3259067cff3b404ab401dc11
lena jurkiewicz <lena.jurkiewicz@fancyfon.com>
2015-11-23 18:00:14 +0100

    Add CallerVerificator interface
    Change-Id: I1fc3cc77a2f856e20bbb98ebc0db36b8303fe0d8

---
+++ src/main/java/com/fancyfon/mobile/android/verification/CallerVerificator.java
@@ -0,0 +1,9 @@
+package com.fancyfon.mobile.android.verification;
+
+/**
+ * @author: Lena Jurkiewicz <lena.jurkiewicz@fancyfon.com>
+ */
+public interface CallerVerificator {
+
+    public boolean isCallerPermitted( int callingId);
+}
